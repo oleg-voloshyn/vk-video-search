4807a86b115337e8304c0c3e1cdade3b8a0716d3
Oleg Voloshyn <voloshyn@active-bridge.com>
2016-06-02 22:05:47 +0300

    add simple video search

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require('sinatra')
 require('sinatra/reloader')
+require('vkontakte_api')
+require('./lib/figaro')
 
-get("/") do
+before do
+  @vk = VkontakteApi::Client.new(Figaro.env.vk_token)
+end
+
+get('/') do
   slim(:index)
+end
+
+post('/videos') do
+  description = params.fetch('description')
+  @result = @vk.video.search(q: description)
+  slim(:result)
 end
---
+++ lib/figaro.rb
@@ -0,0 +1,18 @@
+require('figaro')
+
+module ::Figaro
+  class SinatraApp < Figaro::Application
+    private
+
+    def default_path
+      File.join('config', 'application.yml')
+    end
+
+    def default_environment
+      ENV['RACK_ENV']
+    end
+
+  end
+end
+Figaro.adapter = Figaro::SinatraApp
+Figaro.load

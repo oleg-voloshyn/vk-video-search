4e761a17c319b4161f189aa57bb8473f99624a1c
Oleg Voloshyn <voloshyn@active-bridge.com>
2016-06-03 12:26:32 +0300

    add links for audio and video searches

--- app.rb
+++ app.rb
@@ -11,6 +11,14 @@ get('/') do
   slim(:index)
 end
 
+get('/audios') do
+  slim(:audios)
+end
+
+get('/videos') do
+  slim(:videos)
+end
+
 post('/videos') do
   @result = @vk.video.search(params)
   slim(:result)
